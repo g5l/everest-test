cd3b263517cee799ef6032d9585f7e7371211b0c
Gabriel Debona <gabriel.debona@gmail.com>
2024-08-06 15:58:51 -0300

    feat: added prettier and generators

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,15 @@
-import { FC } from "react";
-import everestLogo from "/everest-logo.svg";
-import "./App.css";
+import everestLogo from '/everest-logo.svg'
+import { FC } from 'react'
+import './App.css'
 
-const App: FC = () => (
-  <div className="everest-app">
-    <img className="everest-app__logo" src={everestLogo} alt="Everest logo" />
-    <h1 className="everest-app__title">TODO App Challenge</h1>
-    <p className="everest-app__docs">Read the instructions to get started!</p>
-  </div>
-);
+const App: FC = () => {
+  return (
+    <div className="everest-app">
+      <img className="everest-app__logo" src={everestLogo} alt="Everest logo" />
+      <h1 className="everest-app__title">TODO App Challenge</h1>
+      <p className="everest-app__docs">Read the instructions to get started!</p>
+    </div>
+  )
+}
 
-export default App;
+export default App
