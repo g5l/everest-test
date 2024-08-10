d5749640582a7c692109f760678b823b956a34a2
Gabriel Debona <gabriel.debona@gmail.com>
2024-08-10 20:18:43 -0300

    fix: Added mobile test to todo.cy

--- cypress/e2e/todo/todo.cy.ts
+++ cypress/e2e/todo/todo.cy.ts
@@ -5,34 +5,48 @@ import {
 } from '../../selector/home'
 
 describe('Todo', () => {
-  beforeEach(() => {
-    cy.visit('/')
-    cy.loadStorage()
-    cy.wait(1000)
+  const shouldRunTest = () => {
+    beforeEach(() => {
+      cy.visit('/')
+      cy.loadStorage()
+      cy.wait(1000)
+    })
+
+    afterEach(() => {
+      cy.saveStorage()
+    })
+
+    const todoItem = 'Walk the dog'
+
+    it('Should add an item to the list', () => {
+      cy.get(headerSelector.input).type(todoItem)
+      cy.get(headerSelector.button).click()
+      cy.wait(1000)
+      cy.get(listSelector.item).contains(todoItem)
+    })
+
+    it('Should search for a todo', () => {
+      cy.get(filtersSelector.search).type(todoItem)
+      cy.wait(1000)
+      cy.get(listSelector.item).contains(todoItem)
+    })
+
+    it('Should search for an item that does not exist', () => {
+      cy.get(filtersSelector.search).type('not found')
+      cy.get(listSelector.item).should('not.exist')
+      cy.get(listSelector.emptyState).should('be.visible')
+    })
+  }
+
+  describe('Desktop', () => {
+    shouldRunTest()
   })
 
-  afterEach(() => {
-    cy.saveStorage()
-  })
+  describe('Mobile', () => {
+    beforeEach(() => {
+      cy.viewport(360, 640)
+    })
 
-  const todoItem = 'Walk the dog'
-
-  it('Should add an item to the list', () => {
-    cy.get(headerSelector.input).type(todoItem)
-    cy.get(headerSelector.button).click()
-    cy.wait(1000)
-    cy.get(listSelector.item).contains(todoItem)
-  })
-
-  it('Should search for a todo', () => {
-    cy.get(filtersSelector.search).type(todoItem)
-    cy.wait(1000)
-    cy.get(listSelector.item).contains(todoItem)
-  })
-
-  it('Should search for an item that does not exist', () => {
-    cy.get(filtersSelector.search).type('not found')
-    cy.get(listSelector.item).should('not.exist')
-    cy.get(listSelector.emptyState).should('be.visible')
+    shouldRunTest()
   })
 })
